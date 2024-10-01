83ad9a548476a5f2dbbd408e4b0831a6512d978e
HardeepSinghh9 <hardeepsinghpallaha@gmail.com>
2024-10-01 14:52:39 +0530

    wpp day 15 ans

---
+++ WPPday15ans.js
@@ -0,0 +1,9 @@
+// Write a JavaScript program to compute the sum of three elements of a given array of integers of length 3.
+
+function sum(nums) {
+  return nums[0] + nums[1] + nums[2];
+}
+
+console.log(sum([1, 8, 10]));
+console.log(sum([10, 10, 20]));
+console.log(sum([40, 10, -11]));
